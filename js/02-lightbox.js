10f8c65532cf9f2e225d6cd755920ae317c8a614
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-19 21:38:24 +0200

    done task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (img) =>
+      `<a class="gallery__item" href="${img.original}">       
+        <img 
+          class="gallery__image" 
+          src=${img.preview}          
+          alt="${img.description}"
+          >
+      </a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
 console.log(galleryItems);
+
+var lightbox = new SimpleLightbox(".gallery a", { 
+  captionsData: "alt",
+  captionDelay: 250,
+});
