d1c8c863da0fe8a6325341d1505cdfe282c6ce13
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-19 15:11:57 +0200

    done task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (img) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${img.original}">
+          <img 
+            class="gallery__image" 
+            src=${img.preview}
+            data-source="${img.original}" 
+            alt="${img.description}"
+            >
+        </a>
+      </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
 console.log(galleryItems);
+
+const viewBigImg = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let bigImgSrc = e.target.dataset.source;
+  basicLightbox
+    .create(
+      `
+      <img width="1280" height="854" src=${bigImgSrc}>
+    `
+    )
+    .show();
+};
+
+gallery.addEventListener("click", viewBigImg);
